8df415f9c96723b4a7d57fd0977ee654196788fa
Jean Galant Yombouno <jeangalanty@gmail.com>
2024-06-05 12:56:27 +0300

    title

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "History Quiz",
+  title: "Financial Market Quiz",
   description: "Made with heart by jgigrec323",
 };
 
